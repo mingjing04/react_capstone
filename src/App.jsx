dd6fcaaf2f6deee11d7fec0030d279da004281e3
Mingjing Song <42714051+mingjing04@users.noreply.github.com>
2024-11-24 16:38:17 +0800

    initialize

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='h-screen bg-green-400'>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore itaque quaerat tenetur aut delectus? Neque temporibus animi adipisci expedita repellat sequi architecto dignissimos odit beatae, incidunt nam, illum veritatis modi.</p>
+
+    </div>
+  )
+}
+
+export default App
